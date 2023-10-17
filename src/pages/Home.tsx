8452e4e50c938c01f21deffdb282fd8c00e59026
Rodal 95 <alday559@gmail.com>
2023-10-16 21:34:08 -0300

    entregando tp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonLabel, IonSelect, IonButton, IonList, IonItem, IonListHeader, IonRow, IonCol, IonSelectOption } from '@ionic/react';
+
+
+import {useState, } from "react"
 import './Home.css';
+import React from 'react';
 
 const Home: React.FC = () => {
 
@@ -20,7 +23,62 @@ const Home: React.FC = () => {
   const redirectComputadoraPage = ()=>{
     window.location.href="/computadora";
   }
+  // Declaración e inicialización de un array de números
+  const numbers: number[] = [1, 2, 3, 4, 5];
+
+  // Mostrar el array en Ionic
+  console.log(numbers);
+
+  const frutas: string[] = ['manzana', 'banana', 'cereza'];
+
+  // Agregar un elemento al final del array
+  frutas.push('uva');
+
+  // Mostrar el array en Ionic
+  console.log(frutas);
   
+  // Declaración e inicialización de un array de números
+  const numeros: number[] = [10, 20, 30, 40, 50];
+
+  // Recorrer el array y mostrar los elementos
+    numeros.forEach(numero => {
+    console.log(numero);
+});
+  // Declaración e inicialización de un array de números
+const valores: number[] = [8, 15, 12, 7, 24, 35];
+
+// Filtrar valores mayores que 10
+const valoresMayoresA10 = valores.filter(valor => valor > 10);
+
+// Mostrar los valores filtrados en Ionic
+console.log(valoresMayoresA10);
+
+interface Persona {
+  Nombre: string;
+  Apellido: string;
+  Edad: number;
+  Estado: 'activo' | 'inactivo';
+}
+
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [edad, setEdad] = useState(0);
+  const [estado, setEstado] = useState<'activo' | 'inactivo'>('activo');
+  const [personas, setPersonas] = useState<Persona[]>([]);
+
+  const agregarPersona = () => {
+    const nuevaPersona: Persona = {
+      Nombre: nombre,
+      Apellido: apellido,
+      Edad: edad,
+      Estado: estado,
+    };
+    setPersonas([...personas, nuevaPersona]);
+    setNombre('');
+    setApellido('');
+    setEdad(0);
+    setEstado('activo');
+  };
 
   return (
     <IonContent>
@@ -47,7 +105,56 @@ const Home: React.FC = () => {
       <IonButton  onClick={redirectComputadoraPage}>
         Ir a computadora page
       </IonButton>
-      
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          agregarPersona();
+        }}>
+          <IonList>
+            <IonListHeader>Agregar Persona</IonListHeader>
+            <IonItem>
+              <IonLabel position="floating">Nombre</IonLabel>
+              <IonInput type="text" value={nombre} onIonChange={(e) => setNombre(e.detail.value!)}></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Apellido</IonLabel>
+              <IonInput type="text" value={apellido} onIonChange={(e) => setApellido(e.detail.value!)}></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="floating">Edad</IonLabel>
+              <IonInput type="number" value={edad} onIonChange={(e) => setEdad(parseInt(e.detail.value!))}></IonInput>
+            </IonItem>
+            <IonItem>
+              <IonLabel>Estado</IonLabel>
+              <IonSelect value={estado} onIonChange={(e) => setEstado(e.detail.value)}>
+                <IonSelectOption value="activo">Activo</IonSelectOption>
+                <IonSelectOption value="inactivo">Inactivo</IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonButton expand="full" type="submit">Agregar</IonButton>
+          </IonList>
+        </form>
+
+        <IonList>
+          <IonListHeader>Lista de Personas</IonListHeader>
+          {personas.map((persona, index) => (
+            <IonRow key={index}>
+              <IonCol>
+                <IonItem>
+                  <IonLabel>Nombre: {persona.Nombre}</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>Apellido: {persona.Apellido}</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>Edad: {persona.Edad}</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>Estado: {persona.Estado}</IonLabel>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+          ))}
+        </IonList>
     </IonContent>
   );
 };
